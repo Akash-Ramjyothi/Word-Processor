ab4b7d1bdf3c6b93587b38cbcbd23e6713c30e82
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2024-08-09 19:18:51 +0530

    Completed Word processor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+export function processWord(input){
+  const charCount = {};
+  for(let char of input){
+    charCount[char] = (charCount[char] || 0)+1;
+  }
+
+  const sortedChars = Object.keys(charCount).sort((a,b)=> charCount[b]-charCount[a]); 
+
+  let result='';
+  for(let char of sortedChars){
+    result+=char.repeat(charCount[char]);
+  }
+
+  return result;
+}
+
 function App() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleChange = (event) => {
+    const newValue = event.target.value;
+    setInput(newValue);
+    setOutput(processWord(newValue));
+  };
+
+  console.log("Input: "+input);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Word Processor</h1>
+      <input type="text"
+      value = {input}
+      onChange={handleChange}
+      placeholder='Enter processing word'
+      ></input>
+      <br />
+      <label>Processed Word: {output}</label>
     </div>
   );
 }
